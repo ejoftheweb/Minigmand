178da0a6e0ddd8ac4f922709351a4d53ada652a2
ejoftheweb <edward.barrow@platosys.co.uk>
2018-02-13 17:19:33 +0000

    commit3 - update to MIT license

--- app/src/main/java/uk/co/platosys/minigma/utils/Kidney.java
+++ app/src/main/java/uk/co/platosys/minigma/utils/Kidney.java
@@ -1,15 +1,24 @@
 /*
  * Copyright Edward Barrow and Platosys.
- * This software is licensed under the Free Software Foundation's
-General Public Licence, version 2 ("the GPL").
-The full terms of the licence can be found online at http://www.fsf.org/
-
-In brief, you are free to copy and to modify the code in any way you wish, but if you
-publish the modified code you may only do so under the GPL, and (if asked) you must
- supply a copy of the source code alongside any compiled code.
-
-Platosys software can also be licensed on negotiated terms if the GPL is inappropriate.
-For further information about this, please contact software.licensing@platosys.co.uk
+ ** (c) copyright 2018 Platosys
+        * MIT Licence
+        * Permission is hereby granted, free of charge, to any person obtaining a copy
+        * of this software and associated documentation files (the "Software"), to deal
+        * in the Software without restriction, including without limitation the rights
+        * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        * copies of the Software, and to permit persons to whom the Software is
+        * furnished to do so, subject to the following conditions:
+        *
+         *The above copyright notice and this permission notice shall be included in all
+         * copies or substantial portions of the Software.
+         *
+         * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+         * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+         * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+         * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+         * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+         * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+         * SOFTWARE.
  */
 package uk.co.platosys.minigma.utils;
 
