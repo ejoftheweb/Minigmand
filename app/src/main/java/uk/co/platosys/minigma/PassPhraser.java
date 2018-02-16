cd1b7c1adbbc300ff1df21407a1cc36f848809e2
edward <edward.barrow@platosys.co.uk>
2018-02-16 21:46:14 +0000

    commitUK1-androidify Exeptions class

--- app/src/main/java/uk/co/platosys/minigma/PassPhraser.java
+++ app/src/main/java/uk/co/platosys/minigma/PassPhraser.java
@@ -7,6 +7,9 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
+import uk.co.platosys.effwords.Effwords;
+import uk.co.platosys.minigma.exceptions.Exceptions;
+
 public class PassPhraser {
     /**
      *  in the Minigma library, a Key is unlocked with a Passphrase, which is a char array.
@@ -22,15 +25,13 @@ public class PassPhraser {
      */
     private  File wordListFile;
     private List<String> wordList;
-    public  int WORDLIST_SIZE=1297;
+    public  static int WORDLIST_SIZE=1297;
     public static final String WORDSEPARATOR = " ";
 
     public PassPhraser(){
-        //System.out.println("running constructor");
-        this.wordListFile=new File("/home/edward/platosys/minigma/resources/wordlist");
-        this.wordList=loadWordList();
+
     }
-    public char[] getPassPhrase(int words) {
+    public static char[] getPassPhrase(int words) {
         SecureRandom secureRandom = new SecureRandom();
         StringBuffer buffer= new StringBuffer();
         for (int i = 0; i < words; i++) {
@@ -39,31 +40,14 @@ public class PassPhraser {
                 if (i > 0) {
                     buffer.append(WORDSEPARATOR);
                 }
-                buffer.append(wordList.get(word));
+                buffer.append(Effwords.getWord(Effwords.EFF_DEFAULTLIST,word));
             }catch (Exception x) {
-                //probably an array out of bounds one
+                Exceptions.dump(x);
             }
         }
         return buffer.toString().toCharArray();
     }
 
-    private  List<String> loadWordList() {
-        List<String> wordList=new ArrayList<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader( new FileReader(wordListFile));
-            String line;
-            while ((line=bufferedReader.readLine())!=null){
-                //System.out.println((line));
-                String word = line.split("\t")[1];
-                wordList.add(word);
-                //System.out.println(word);
-            }
 
-
-        }catch(Exception x){
-            System.out.println(x.getClass()+"\n"+x.getMessage());
-        }
-        return wordList;
-    }
 }
 
