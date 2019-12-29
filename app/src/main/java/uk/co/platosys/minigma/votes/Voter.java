485783f3aaf07fee471e121f8e28e4e7c89635e4
edward <edward.barrow@platosys.co.uk>
2019-12-29 18:24:40 +0100

    Merge branch 'master' of https://github.com/ejoftheweb/Minigmand
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle
    #	app/src/main/java/uk/co/platosys/minigma/BaseSignature.java
    #	app/src/main/java/uk/co/platosys/minigma/exceptions/Exceptions.java
    #	app/src/test/java/uk/co/platosys/minigma/CertificationTest.java
    #	app/src/test/java/uk/co/platosys/minigma/EncryptionDecryptionTest.java
    #	app/src/test/java/uk/co/platosys/minigma/LockSmithTest.java
    #	app/src/test/java/uk/co/platosys/minigma/LockStoreTest.java
    #	app/src/test/java/uk/co/platosys/minigma/PassPhraserTest.java
    #	app/src/test/java/uk/co/platosys/minigma/TestValues.java
    #	app/src/test/java/uk/co/platosys/minigma/VerifySignatureTest.java

--- app/src/main/java/uk/co/platosys/minigma/votes/Voter.java
+++ app/src/main/java/uk/co/platosys/minigma/votes/Voter.java
@@ -18,13 +18,14 @@ public interface Voter {
      BigBinary getPaper();
 
      /**
-      * Returns the signature of the Returning Officer as applied to the
+      * Returns the signature of the Returning Officer as applied to the voter's ballot.
+
       * @return
       */
      Signature getPollSignature();
 
      /**Called by the Poll object when a poll is called. The BigBinary passed as an argument is
-      * the Ballot, encrypted with the Voter's public. It is accompanied by the Signature of the
+      * the Ballot, encrypted with the Voter's public Lock. It is accompanied by the Signature of the
       * poll's Returning Officer. It is up to you how you actually get it to
       * the voters - email, or through your application's preferred messaging system.  The voter must then
       * decrypt and verify the ballot, append his or her vote (which is an int) and then re-encrypt it with the
