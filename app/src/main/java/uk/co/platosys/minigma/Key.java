619bc279fff38cfd80194552f2a6f82657966110
edward <edward.barrow@platosys.co.uk>
2020-02-06 11:07:36 +0000

    20200206 added changePassphrase method to Key

--- app/src/main/java/uk/co/platosys/minigma/Key.java
+++ app/src/main/java/uk/co/platosys/minigma/Key.java
@@ -23,18 +23,27 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.spongycastle.bcpg.ArmoredInputStream;
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+import org.spongycastle.openpgp.PGPException;
 import org.spongycastle.openpgp.PGPSecretKey;
 import org.spongycastle.openpgp.PGPSecretKeyRing;
 import org.spongycastle.openpgp.PGPSecretKeyRingCollection;
 import org.spongycastle.openpgp.PGPUtil;
 import org.spongycastle.openpgp.operator.KeyFingerPrintCalculator;
+import org.spongycastle.openpgp.operator.PBESecretKeyDecryptor;
+import org.spongycastle.openpgp.operator.PBESecretKeyEncryptor;
 import org.spongycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.spongycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
+import org.spongycastle.openpgp.operator.jcajce.JcePBESecretKeyEncryptorBuilder;
 
 import uk.co.platosys.minigma.exceptions.BadPassphraseException;
 import uk.co.platosys.minigma.exceptions.MinigmaException;
@@ -72,6 +81,7 @@ public class Key {
     private PGPSecretKeyRingCollection secretKeyRingCollection;
     private Fingerprint fingerprint;
     private String userID="";
+    private File file;
 
 
     /** @param secretKeyRingCollection
@@ -83,10 +93,12 @@ public class Key {
 
 
     /** @param keyFile  a java.io.File object pointing to  a text file of OpenPGP key material
+     *                   Keys instantiated using this method will not be able to return a meaningful
+     *                  userID
      */
-
     public Key(File keyFile)throws MinigmaException {
         try{
+            this.file=keyFile;
             FileInputStream fileStream=new FileInputStream(keyFile);
             InputStream instream=new ArmoredInputStream(fileStream);
             instream=PGPUtil.getDecoderStream(instream);
@@ -99,6 +111,13 @@ public class Key {
             throw new MinigmaException("problem loading Key from file", x);
         }
     }
+
+    /**Keys can also be instantiated from an InputStream rather than a File. (although why??)
+     *
+     * @param inputStream
+     * @throws MinigmaException
+     */
+    @Deprecated
     public Key(InputStream inputStream)throws MinigmaException {
         try{
             InputStream instream=new ArmoredInputStream(inputStream);
@@ -112,11 +131,13 @@ public class Key {
             throw new MinigmaException("problem loading Key from input stream", x);
         }
     }
-    /** @param keyFile  a java.io.File object pointing to  a text file of OpenPGP key material
+    /** @param keyFile  a java.io.File object pointing to  a text file of OpenPGP key material. This constructor
+     *                   takes a LockStore argument, to enable lookup of a meaningful userID
      */
 
     public Key(File keyFile, LockStore lockStore)throws MinigmaException {
         try{
+            this.file=keyFile;
             FileInputStream fileStream=new FileInputStream(keyFile);
             InputStream instream=new ArmoredInputStream(fileStream);
             instream=PGPUtil.getDecoderStream(instream);
@@ -129,6 +150,7 @@ public class Key {
             throw new MinigmaException("problem loading Key from file", x);
         }
     }
+    @Deprecated
     public Key(InputStream inputStream, LockStore lockStore)throws MinigmaException {
         try{
             InputStream instream=new ArmoredInputStream(inputStream);
@@ -171,24 +193,56 @@ public class Key {
             throw e;
         }
     }
-    /** @return the keyID for this key;*/
-    public Fingerprint getFingerprint(){return fingerprint;}
+    /** @return the Fingerprint object associated with this Key;*/
+    public Fingerprint getFingerprint(){
+        return fingerprint;
+    }
+    /** @return the 64-bit keyID associated with this Key. */
     public long getKeyID(){
-
         return fingerprint.getKeyID();
     }
     /** @return the primary userID associated with this key;
-     * or the empty string*/
+     * or the empty string */
     public String getUserID() {return userID;}
 
     /**
      *
      * @return
      */
-    protected PGPSecretKey getSigningKey(){
-        return signingKey;
-    }
+    protected PGPSecretKey getSigningKey(){return signingKey;}
     protected PGPSecretKey getMasterKey(){return masterKey;}
+
+    /**
+     * Changes the passphrase of this Key.
+     */
+    public void changePassphrase(char[] oldpassphrase, char[] newpassphrase) throws MinigmaOtherException, BadPassphraseException {
+        PBESecretKeyDecryptor decryptor=null;
+        PBESecretKeyEncryptor encryptor=null;
+        List<PGPSecretKeyRing> newkeys = new ArrayList<>();
+        try {
+            decryptor = new JcePBESecretKeyDecryptorBuilder().setProvider(BouncyCastleProvider.PROVIDER_NAME).build(oldpassphrase);
+            encryptor = new JcePBESecretKeyEncryptorBuilder(Algorithms.SYMMETRIC_ALGORITHM).setProvider(BouncyCastleProvider.PROVIDER_NAME).build(newpassphrase);
+        }catch(PGPException px){
+            throw new MinigmaOtherException("error creating passphrase encryptor/decryptor", px);
+        }
+        Iterator<PGPSecretKeyRing> ringIterator = secretKeyRingCollection.iterator(); //TODO
+        while(ringIterator.hasNext()){
+            PGPSecretKeyRing secretKeyRing = ringIterator.next();
+            try {
+                newkeys.add(PGPSecretKeyRing.copyWithNewPassword(secretKeyRing, decryptor, encryptor));
+            }catch(PGPException px){
+                throw new BadPassphraseException("probably", px);
+            }
+        }
+        try {
+            secretKeyRingCollection = new PGPSecretKeyRingCollection(newkeys);
+            secretKeyRingCollection.encode(new FileOutputStream(file));
+        }catch(IOException iox){
+            //TODO
+        }catch (PGPException px){
+            //TODO
+        }
+    }
 
     /**
      *Returns an BouncyCastle PGPSecretKey decryption key, to be used to
